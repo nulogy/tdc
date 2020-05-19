23e9aeebb20f598450d7d362c6d9d7cdf9b9554a
Alistair McKinnell <alistairm@nulogy.com>
2020-05-19 07:43:27 -0400

    Introduce the DataDefinition base class

---
+++ lib/tdc/data_definition.rb
@@ -0,0 +1,14 @@
+module Tdc
+  #
+  # Knows how to read data definitions from the specified path elements.
+  #
+  class DataDefinition
+    def read(*path_elements)
+      raise MissingOverrideError
+    end
+
+    def with_indifferent_access
+      self.extend(Tdc::WithIndifferentAccessDecorator) # rubocop:disable Style/RedundantSelf
+    end
+  end
+end
