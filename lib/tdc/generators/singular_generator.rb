4feedba027b5eab1af5977a858f60782ded74eb8
Alistair McKinnell <alistairm@nulogy.com>
2020-07-19 08:37:06 -0400

    Simplify
    - Add the #additional_definitions method
    - Avoid having to chain initialize methods

--- lib/tdc/generators/singular_generator.rb
+++ lib/tdc/generators/singular_generator.rb
@@ -6,12 +6,6 @@ module Tdc
     # See also StandardGenerator.
     #
     class SingularGenerator < Tdc::Generators::ConfigurableGenerator
-      def initialize(data_definition, current_catalog)
-        super
-
-        @additional_definitions = {}
-      end
-
       def with_definition(additional_definitions)
         @additional_definitions = additional_definitions.stringify_keys.reject { |_, v| v == :missing_definition }
 
@@ -19,12 +13,16 @@ module Tdc
       end
 
       def generate
-        configure_instance_definition(singular_instance_definition.merge(@additional_definitions))
+        configure_instance_definition(singular_instance_definition.merge(additional_definitions))
 
         run_resolvers_and_generate_instance
       end
 
       private
+
+      def additional_definitions
+        @additional_definitions || {}
+      end
 
       def singular_instance_definition
         all_instance_definitions = instance_definitions
