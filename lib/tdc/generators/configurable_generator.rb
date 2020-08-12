797341dc906a9f4247221f23187b379f36be762d
Alistair McKinnell <alistairm@nulogy.com>
2020-08-12 16:24:20 -0400

    Collapse the ConfigurableGenerator into the StandardGenerator

--- lib/tdc/generators/configurable_generator.rb
+++ lib/tdc/generators/configurable_generator.rb
@@ -3,12 +3,28 @@ module Tdc
     #
     # Knows how to provide a configurable instance definition.
     #
-    class ConfigurableGenerator < Tdc::Generators::GeneratorBase
+    class StandardGenerator < Tdc::Generators::GeneratorBase
       include Tdc::Generators::DefinitionSourcable
 
       attr_reader :instance_definition
 
       source_definition_from :instance_definition
+
+      def generate
+        CatalogEntries.new.tap do |catalog_entries|
+          instance_definitions.each do |instance_definition|
+            configure_instance_definition(instance_definition)
+
+            # Delete the tag so that the models do not need to filter it out.
+            tag = instance_definition.delete("tag")
+
+            generated = run_resolvers_and_generate_instance
+
+            # Add generated instance to the catalog entries whenever a tag provided.
+            catalog_entries.add_catalog_entry(tag, generated) if tag
+          end
+        end
+      end
 
       def run_resolvers_and_generate_instance
         run_extended_attribute_interpreters(instance_definition)
