44b3d09a8a6a74145ec6b5be08b018a885effb3f
Alistair McKinnell <alistairm@nulogy.com>
2020-08-25 13:57:45 -0400

    Support optional definitions

--- spec/shared_examples/generators/definition_sourceable_shared_example.rb
+++ spec/shared_examples/generators/definition_sourceable_shared_example.rb
@@ -19,6 +19,38 @@ RSpec.shared_examples "includes DefinitionSourcable" do
     expect(generator.results).to eq([["Manning", 2.0]])
   end
 
+  it "nil for a missing optional definition" do
+    instance_definitions = [
+      instance_definition(tag: "manning", name: "Manning", wait: 2.0)
+    ]
+
+    generator.inject_instance_definitions(instance_definitions)
+
+    # Inject a spy so that we can sense when the #generate_instance method is called.
+    generator.define_singleton_method :generate_instance do
+      add_result([name_definition, wait_definition, missing_definition_optional])
+    end
+
+    generator.generate
+
+    expect(generator.results).to eq([["Manning", 2.0, nil]])
+  end
+
+  it "raises for a missing definition" do
+    instance_definitions = [
+      instance_definition(tag: "manning", name: "Manning", wait: 2.0)
+    ]
+
+    generator.inject_instance_definitions(instance_definitions)
+
+    # Inject a spy so that we can sense when the #generate_instance method is called.
+    generator.define_singleton_method :generate_instance do
+      add_result([name_definition, wait_definition, missing_definition])
+    end
+
+    expect { generator.generate }.to raise_error(KeyError)
+  end
+
   it "tags are removed" do
     instance_definitions = [
       instance_definition(tag: "manning", name: "Manning", wait: 2.0)
@@ -31,6 +63,6 @@ RSpec.shared_examples "includes DefinitionSourcable" do
       add_result([tag_definition])
     end
 
-    expect { generator.generate }.to raise_error(NameError)
+    expect { generator.generate }.to raise_error(KeyError)
   end
 end
