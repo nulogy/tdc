fc1a0198298e5d6df0d9d85b90aac3fb682f422f
Alistair McKinnell <alistairm@nulogy.com>
2025-10-25 00:19:54 -0400

    PM-16232: Address RSpec/IncludeExamples violation

--- spec/tdc/generators/standard_generator_spec.rb
+++ spec/tdc/generators/standard_generator_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Tdc::Generators::StandardGenerator, :tdc do
 
   subject(:generator) { Support::TdcFixture::SampleStandardGenerator.new(generation_context) }
 
-  include_examples "includes DefinitionSourcable"
+  it_behaves_like "includes DefinitionSourcable"
 
   it "generates" do
     instance_definitions = [
